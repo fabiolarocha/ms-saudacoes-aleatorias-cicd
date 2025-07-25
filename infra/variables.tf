af9dc6c396a27af79a5c1fa344ebf2f9f55b326a
Fabíola Rocha <fabiola.rocha@outlook.com.br>
2025-07-25 15:41:45 -0300

    fix: declara docker_user_name em variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,11 @@
 variable "app_name" {
   type    = string
-  default = "ms-saudacoes-aleatorias"
+  default = "ms-saudacoes-aleatorias-app"
 }
 
 variable "service_name" {
   type    = string
-  default = "saudacoes-aleatorias"
+  default = "ms-saudacoes-aleatorias-service"
 }
 
 variable "instance_type" {
@@ -18,13 +18,18 @@ variable "container_port" {
   default = 8080
 }
 
+variable "docker_user_name" {
+  description = "fabiolarochauft"
+  type        = string
+  sensitive   = false # Não é sensível se for apenas o nome de usuário
+}
+
 variable "docker_image_name" {
   type    = string
-  default = "xxxxxx/ms-saudacoes-aleatorias"
+  default = "ms-saudacoes-aleatorias"
 }
 
 variable "docker_image_tag" {
   type    = string
   default = "latest"
 }
-  
